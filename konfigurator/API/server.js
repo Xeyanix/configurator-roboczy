1afdaf196034502c87007dbbbe5a91b1e66bc5ff
Xeyanix <jan.goralewski@o2.pl>
2025-04-26 03:52:40 +0200

    Dodano katalog konfigurator jako zwykły katg

--- konfigurator/API/server.js
+++ konfigurator/API/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const res = require('express/lib/response');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = process.env.PORT || 9000;
